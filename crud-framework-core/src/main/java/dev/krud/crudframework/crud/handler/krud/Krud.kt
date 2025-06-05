bb504bd1d9991f1abb94aa1c00a2fee783deee94
Shani Holdengreber <39795021+thewizkid87@users.noreply.github.com>
2025-06-05 10:33:53 -0500

    task: Expose inline hooks in Krud interface (#22)

--- crud-framework-core/src/main/java/dev/krud/crudframework/crud/handler/krud/Krud.kt
+++ crud-framework-core/src/main/java/dev/krud/crudframework/crud/handler/krud/Krud.kt
@@ -1,5 +1,24 @@
 package dev.krud.crudframework.crud.handler.krud
 
+import dev.krud.crudframework.crud.hooks.HooksDTO
+import dev.krud.crudframework.crud.hooks.create.CRUDOnCreateHook
+import dev.krud.crudframework.crud.hooks.create.CRUDPostCreateHook
+import dev.krud.crudframework.crud.hooks.create.CRUDPreCreateHook
+import dev.krud.crudframework.crud.hooks.delete.CRUDOnDeleteHook
+import dev.krud.crudframework.crud.hooks.delete.CRUDPostDeleteHook
+import dev.krud.crudframework.crud.hooks.delete.CRUDPreDeleteHook
+import dev.krud.crudframework.crud.hooks.index.CRUDOnIndexHook
+import dev.krud.crudframework.crud.hooks.index.CRUDPostIndexHook
+import dev.krud.crudframework.crud.hooks.index.CRUDPreIndexHook
+import dev.krud.crudframework.crud.hooks.show.CRUDOnShowHook
+import dev.krud.crudframework.crud.hooks.show.CRUDPostShowHook
+import dev.krud.crudframework.crud.hooks.show.CRUDPreShowHook
+import dev.krud.crudframework.crud.hooks.show.by.CRUDOnShowByHook
+import dev.krud.crudframework.crud.hooks.show.by.CRUDPostShowByHook
+import dev.krud.crudframework.crud.hooks.show.by.CRUDPreShowByHook
+import dev.krud.crudframework.crud.hooks.update.CRUDOnUpdateHook
+import dev.krud.crudframework.crud.hooks.update.CRUDPostUpdateHook
+import dev.krud.crudframework.crud.hooks.update.CRUDPreUpdateHook
 import dev.krud.crudframework.model.BaseCrudEntity
 import dev.krud.crudframework.modelfilter.DynamicModelFilter
 import dev.krud.crudframework.modelfilter.dsl.FilterFieldsBuilder
@@ -11,28 +30,28 @@ import java.io.Serializable
 interface Krud<Entity : BaseCrudEntity<ID>, ID : Serializable> {
     val entityClazz: Class<Entity>
 
-    fun create(entity: Entity, applyPolicies: Boolean = false): Entity
+    fun create(entity: Entity, applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreCreateHook<ID, Entity>, CRUDOnCreateHook<ID, Entity>, CRUDPostCreateHook<ID, Entity>> = noHooks()): Entity
 
     fun bulkCreate(entities: List<Entity>, applyPolicies: Boolean): List<Entity>
 
-    fun showById(id: ID, cached: Boolean = false, persistCopy: Boolean? = null, applyPolicies: Boolean = false): Entity?
+    fun showById(id: ID, cached: Boolean = false, persistCopy: Boolean? = null, applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreShowHook<ID, Entity>, CRUDOnShowHook<ID, Entity>, CRUDPostShowHook<ID, Entity>> = noHooks()): Entity?
 
-    fun showByFilter(filter: DynamicModelFilter, cached: Boolean = false, persistCopy: Boolean? = null, applyPolicies: Boolean = false): Entity?
+    fun showByFilter(filter: DynamicModelFilter, cached: Boolean = false, persistCopy: Boolean? = null, applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreShowByHook<ID, Entity>, CRUDOnShowByHook<ID, Entity>, CRUDPostShowByHook<ID, Entity>> = noHooks()): Entity?
 
-    fun showByFilter(cached: Boolean = false, persistCopy: Boolean? = null, applyPolicies: Boolean = false, block: ModelFilterBuilder<Entity>.() -> Unit): Entity? {
+    fun showByFilter(cached: Boolean = false, persistCopy: Boolean? = null, applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreShowByHook<ID, Entity>, CRUDOnShowByHook<ID, Entity>, CRUDPostShowByHook<ID, Entity>> = noHooks(), block: ModelFilterBuilder<Entity>.() -> Unit): Entity? {
         val builder = ModelFilterBuilder<Entity>()
         builder.block()
         val filter = builder.build()
-        return showByFilter(filter, cached, persistCopy, applyPolicies)
+        return showByFilter(filter, cached, persistCopy, applyPolicies, hooks)
     }
 
-    fun searchByFilter(filter: DynamicModelFilter, cached: Boolean = false, persistCopy: Boolean? = null, applyPolicies: Boolean = false): PagedResult<Entity>
+    fun searchByFilter(filter: DynamicModelFilter, cached: Boolean = false, persistCopy: Boolean? = null, applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreIndexHook<ID, Entity>, CRUDOnIndexHook<ID, Entity>, CRUDPostIndexHook<ID, Entity>> = noHooks()): PagedResult<Entity>
 
-    fun searchByFilter(cached: Boolean = false, persistCopy: Boolean? = null, applyPolicies: Boolean = false, block: ModelFilterBuilder<Entity>.() -> Unit): PagedResult<Entity> {
+    fun searchByFilter(cached: Boolean = false, persistCopy: Boolean? = null, applyPolicies: Boolean = false,  hooks: HooksDTO<CRUDPreIndexHook<ID, Entity>, CRUDOnIndexHook<ID, Entity>, CRUDPostIndexHook<ID, Entity>> = noHooks(), block: ModelFilterBuilder<Entity>.() -> Unit): PagedResult<Entity> {
         val builder = ModelFilterBuilder<Entity>()
         builder.block()
         val filter = builder.build()
-        return searchByFilter(filter, cached, persistCopy, applyPolicies)
+        return searchByFilter(filter, cached, persistCopy, applyPolicies, hooks)
     }
 
     fun searchByFilterCount(filter: DynamicModelFilter, applyPolicies: Boolean = false): Long
@@ -44,46 +63,50 @@ interface Krud<Entity : BaseCrudEntity<ID>, ID : Serializable> {
         return searchByFilterCount(filter, applyPolicies)
     }
 
-    fun update(entity: Entity, applyPolicies: Boolean = false): Entity
+    fun update(entity: Entity, applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreUpdateHook<ID, Entity>, CRUDOnUpdateHook<ID, Entity>, CRUDPostUpdateHook<ID, Entity>> = noHooks()): Entity
 
-    fun updateByFilter(filter: DynamicModelFilter, applyPolicies: Boolean = false, updateBlock: Entity.() -> Unit): PagedResult<Entity> {
+    fun updateByFilter(filter: DynamicModelFilter, applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreUpdateHook<ID, Entity>, CRUDOnUpdateHook<ID, Entity>, CRUDPostUpdateHook<ID, Entity>> = noHooks(), updateBlock: Entity.() -> Unit): PagedResult<Entity> {
         return searchByFilter(filter, applyPolicies = applyPolicies).mapResults {
             it.updateBlock()
-            update(it, applyPolicies)
+            update(it, applyPolicies, hooks)
         }
     }
 
-    fun updateByFilter(applyPolicies: Boolean = false, searchBlock: ModelFilterBuilder<Entity>.() -> Unit, updateBlock: Entity.() -> Unit): PagedResult<Entity> {
+    fun updateByFilter(applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreUpdateHook<ID, Entity>, CRUDOnUpdateHook<ID, Entity>, CRUDPostUpdateHook<ID, Entity>> = noHooks(), searchBlock: ModelFilterBuilder<Entity>.() -> Unit, updateBlock: Entity.() -> Unit): PagedResult<Entity> {
         val builder = ModelFilterBuilder<Entity>()
         builder.searchBlock()
         val filter = builder.build()
-        return updateByFilter(filter, applyPolicies, updateBlock)
+        return updateByFilter(filter, applyPolicies, hooks, updateBlock)
     }
 
-    fun updateById(id: ID, applyPolicies: Boolean = false, block: Entity.() -> Unit): Entity {
+    fun updateById(id: ID, applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreUpdateHook<ID, Entity>, CRUDOnUpdateHook<ID, Entity>, CRUDPostUpdateHook<ID, Entity>> = noHooks(), block: Entity.() -> Unit): Entity {
         val entity = showById(id, applyPolicies = applyPolicies) ?: error("Entity with id $id not found")
         entity.block()
-        return update(entity, applyPolicies)
+        return update(entity, applyPolicies, hooks)
     }
 
-    fun bulkUpdate(entities: List<Entity>, applyPolicies: Boolean): List<Entity>
+    fun bulkUpdate(entities: List<Entity>, applyPolicies: Boolean, hooks: HooksDTO<CRUDPreUpdateHook<ID, Entity>, CRUDOnUpdateHook<ID, Entity>, CRUDPostUpdateHook<ID, Entity>> = noHooks()): List<Entity>
 
-    fun deleteById(id: ID, applyPolicies: Boolean = false)
+    fun deleteById(id: ID, applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreDeleteHook<ID, Entity>, CRUDOnDeleteHook<ID, Entity>, CRUDPostDeleteHook<ID, Entity>> = noHooks())
 
-    fun delete(entity: Entity, applyPolicies: Boolean = false) = deleteById(entity.id, applyPolicies)
+    fun delete(entity: Entity, applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreDeleteHook<ID, Entity>, CRUDOnDeleteHook<ID, Entity>, CRUDPostDeleteHook<ID, Entity>> = noHooks()) = deleteById(entity.id, applyPolicies, hooks)
 
-    fun deleteByFilter(filter: DynamicModelFilter, applyPolicies: Boolean = false) {
+    fun deleteByFilter(filter: DynamicModelFilter, applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreDeleteHook<ID, Entity>, CRUDOnDeleteHook<ID, Entity>, CRUDPostDeleteHook<ID, Entity>> = noHooks()) {
         searchByFilter(filter, applyPolicies = applyPolicies).forEach {
-            delete(it, applyPolicies)
+            delete(it, applyPolicies, hooks)
         }
     }
 
-    fun deleteByFilter(applyPolicies: Boolean = false, block: ModelFilterBuilder<Entity>.() -> Unit) {
+    fun deleteByFilter(applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreDeleteHook<ID, Entity>, CRUDOnDeleteHook<ID, Entity>, CRUDPostDeleteHook<ID, Entity>> = noHooks(), block: ModelFilterBuilder<Entity>.() -> Unit) {
         val builder = ModelFilterBuilder<Entity>()
         builder.block()
         val filter = builder.build()
-        deleteByFilter(filter, applyPolicies)
+        deleteByFilter(filter, applyPolicies, hooks)
     }
 
-    fun bulkDelete(ids: List<ID>, applyPolicies: Boolean)
+    fun bulkDelete(ids: List<ID>, applyPolicies: Boolean = false, hooks: HooksDTO<CRUDPreDeleteHook<ID, Entity>, CRUDOnDeleteHook<ID, Entity>, CRUDPostDeleteHook<ID, Entity>> = noHooks())
+
+    companion object {
+        fun <PreHook, OnHook, PostHook> noHooks() = HooksDTO(mutableListOf<PreHook>(), mutableListOf<OnHook>(), mutableListOf<PostHook>())
+    }
 }
--- crud-framework-core/src/main/java/dev/krud/crudframework/crud/handler/krud/KrudImpl.kt
+++ crud-framework-core/src/main/java/dev/krud/crudframework/crud/handler/krud/KrudImpl.kt
@@ -5,6 +5,24 @@ import dev.krud.crudframework.crud.handler.CrudDeleteHandler
 import dev.krud.crudframework.crud.handler.CrudReadHandler
 import dev.krud.crudframework.crud.handler.CrudUpdateHandler
 import dev.krud.crudframework.crud.hooks.HooksDTO
+import dev.krud.crudframework.crud.hooks.create.CRUDOnCreateHook
+import dev.krud.crudframework.crud.hooks.create.CRUDPostCreateHook
+import dev.krud.crudframework.crud.hooks.create.CRUDPreCreateHook
+import dev.krud.crudframework.crud.hooks.delete.CRUDOnDeleteHook
+import dev.krud.crudframework.crud.hooks.delete.CRUDPostDeleteHook
+import dev.krud.crudframework.crud.hooks.delete.CRUDPreDeleteHook
+import dev.krud.crudframework.crud.hooks.index.CRUDOnIndexHook
+import dev.krud.crudframework.crud.hooks.index.CRUDPostIndexHook
+import dev.krud.crudframework.crud.hooks.index.CRUDPreIndexHook
+import dev.krud.crudframework.crud.hooks.show.CRUDOnShowHook
+import dev.krud.crudframework.crud.hooks.show.CRUDPostShowHook
+import dev.krud.crudframework.crud.hooks.show.CRUDPreShowHook
+import dev.krud.crudframework.crud.hooks.show.by.CRUDOnShowByHook
+import dev.krud.crudframework.crud.hooks.show.by.CRUDPostShowByHook
+import dev.krud.crudframework.crud.hooks.show.by.CRUDPreShowByHook
+import dev.krud.crudframework.crud.hooks.update.CRUDOnUpdateHook
+import dev.krud.crudframework.crud.hooks.update.CRUDPostUpdateHook
+import dev.krud.crudframework.crud.hooks.update.CRUDPreUpdateHook
 import dev.krud.crudframework.model.BaseCrudEntity
 import dev.krud.crudframework.modelfilter.DynamicModelFilter
 import dev.krud.crudframework.ro.PagedResult
@@ -22,47 +40,43 @@ open class KrudImpl<Entity : BaseCrudEntity<ID>, ID : Serializable>(
         }
     }
 
-    override fun create(entity: Entity, applyPolicies: Boolean): Entity {
-        return crudCreateHandler.createInternal(entity, noHooks(), applyPolicies)
+    override fun create(entity: Entity, applyPolicies: Boolean, hooks: HooksDTO<CRUDPreCreateHook<ID, Entity>, CRUDOnCreateHook<ID, Entity>, CRUDPostCreateHook<ID, Entity>>): Entity {
+        return crudCreateHandler.createInternal(entity, hooks, applyPolicies)
     }
 
     override fun bulkCreate(entities: List<Entity>, applyPolicies: Boolean): List<Entity> {
         return crudCreateHandler.bulkCreateInternal(entities, applyPolicies)
     }
 
-    override fun showById(id: ID, cached: Boolean, persistCopy: Boolean?, applyPolicies: Boolean): Entity? {
-        return crudReadHandler.showInternal(id, entityClazz, noHooks(), cached, persistCopy, applyPolicies)
+    override fun showById(id: ID, cached: Boolean, persistCopy: Boolean?, applyPolicies: Boolean, hooks: HooksDTO<CRUDPreShowHook<ID, Entity>, CRUDOnShowHook<ID, Entity>, CRUDPostShowHook<ID, Entity>>): Entity? {
+        return crudReadHandler.showInternal(id, entityClazz, hooks, cached, persistCopy, applyPolicies)
     }
 
-    override fun showByFilter(filter: DynamicModelFilter, cached: Boolean, persistCopy: Boolean?, applyPolicies: Boolean): Entity? {
-        return crudReadHandler.showByInternal(filter, entityClazz, noHooks(), cached, persistCopy, applyPolicies)
+    override fun showByFilter(filter: DynamicModelFilter, cached: Boolean, persistCopy: Boolean?, applyPolicies: Boolean, hooks: HooksDTO<CRUDPreShowByHook<ID, Entity>, CRUDOnShowByHook<ID, Entity>, CRUDPostShowByHook<ID, Entity>>): Entity? {
+        return crudReadHandler.showByInternal(filter, entityClazz, hooks, cached, persistCopy, applyPolicies)
     }
 
-    override fun searchByFilter(filter: DynamicModelFilter, cached: Boolean, persistCopy: Boolean?, applyPolicies: Boolean): PagedResult<Entity> {
-        return crudReadHandler.indexInternal(filter, entityClazz, noHooks(), cached, persistCopy, applyPolicies, false)
+    override fun searchByFilter(filter: DynamicModelFilter, cached: Boolean, persistCopy: Boolean?, applyPolicies: Boolean, hooks: HooksDTO<CRUDPreIndexHook<ID, Entity>, CRUDOnIndexHook<ID, Entity>, CRUDPostIndexHook<ID, Entity>>): PagedResult<Entity> {
+        return crudReadHandler.indexInternal(filter, entityClazz, hooks, cached, persistCopy, applyPolicies, false)
     }
 
     override fun searchByFilterCount(filter: DynamicModelFilter, applyPolicies: Boolean): Long {
-        return crudReadHandler.indexInternal(filter, entityClazz, noHooks(), false, false, applyPolicies, true).total
+        return crudReadHandler.indexInternal(filter, entityClazz, Krud.noHooks(), false, false, applyPolicies, true).total
     }
 
-    override fun update(entity: Entity, applyPolicies: Boolean): Entity {
-        return crudUpdateHandler.updateInternal(entity, noHooks(), applyPolicies)
+    override fun update(entity: Entity, applyPolicies: Boolean, hooks: HooksDTO<CRUDPreUpdateHook<ID, Entity>, CRUDOnUpdateHook<ID, Entity>, CRUDPostUpdateHook<ID, Entity>>): Entity {
+        return crudUpdateHandler.updateInternal(entity, hooks, applyPolicies)
     }
 
-    override fun bulkUpdate(entities: List<Entity>, applyPolicies: Boolean): List<Entity> {
-        return crudUpdateHandler.bulkUpdate(entities, noHooks(), applyPolicies)
+    override fun bulkUpdate(entities: List<Entity>, applyPolicies: Boolean, hooks: HooksDTO<CRUDPreUpdateHook<ID, Entity>, CRUDOnUpdateHook<ID, Entity>, CRUDPostUpdateHook<ID, Entity>>): List<Entity> {
+        return crudUpdateHandler.bulkUpdate(entities, hooks, applyPolicies)
     }
 
-    override fun deleteById(id: ID, applyPolicies: Boolean) {
-        crudDeleteHandler.deleteInternal(id, entityClazz, noHooks(), applyPolicies)
+    override fun deleteById(id: ID, applyPolicies: Boolean, hooks: HooksDTO<CRUDPreDeleteHook<ID, Entity>, CRUDOnDeleteHook<ID, Entity>, CRUDPostDeleteHook<ID, Entity>>) {
+        crudDeleteHandler.deleteInternal(id, entityClazz, hooks, applyPolicies)
     }
 
-    override fun bulkDelete(ids: List<ID>, applyPolicies: Boolean) {
-        crudDeleteHandler.bulkDelete(ids, entityClazz, noHooks(), applyPolicies)
-    }
-
-    companion object {
-        private fun <PreHook, OnHook, PostHook> noHooks() = HooksDTO(mutableListOf<PreHook>(), mutableListOf<OnHook>(), mutableListOf<PostHook>())
+    override fun bulkDelete(ids: List<ID>, applyPolicies: Boolean, hooks: HooksDTO<CRUDPreDeleteHook<ID, Entity>, CRUDOnDeleteHook<ID, Entity>, CRUDPostDeleteHook<ID, Entity>>) {
+        crudDeleteHandler.bulkDelete(ids, entityClazz, hooks, applyPolicies)
     }
 }
