9958b98f389071b8eac0d0f6e0febdc73f596276
Idan Elhalwani <idan.elh@gmail.com>
2026-01-13 07:12:54 +0200

    Fix snapshot url

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ if (hasProperty("release")) {
                 username.set(ossrhUsername)
                 password.set(ossrhPassword)
                 nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
-                snapshotRepositoryUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/content/repositories/snapshots/"))
+                snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             }
         }
     }
